cc217cde25512ee9f8c1806a6baca9a4fd19cb24
minsoeaung <minsoeaung2001@gmail.com>
2020-11-09 23:48:04 +0630

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
-import { DISHES } from '../shared/dishes';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 import Constants from 'expo-constants';
 
 
@@ -11,38 +12,48 @@ import Constants from 'expo-constants';
 const MenuNavigator = createStackNavigator({
   Menu: { screen: Menu },
   Dishdetail: { screen: Dishdetail }
-},
-{
-  initialRouteName: 'Menu',
+}, {
   navigationOptions: {
-      headerStyle: {
-          backgroundColor: "#512DA8"
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-          color: "#fff"            
-      }
+    headerStyle: { backgroundColor: "#512DA8" },
+    headerTitleStyle: { color: "#fff" },
+    headerTintColor: '#fff'
   }
-}
-);
+});
+
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: { backgroundColor: "#512DA8" },
+    headerTitleStyle: { color: "#fff" },
+    headerTintColor: "#fff"  
+  })
+});
+
+const MainNavigator = createDrawerNavigator({
+  Home: 
+    { screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home'
+      }
+    },
+  Menu: 
+    { screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu'
+      }, 
+    }
+}, {
+drawerBackgroundColor: '#D1C4E9'
+});
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null
-    };
-  }
-  onDishSelect(dishId){
-      this.setState({selectedDish: dishId})
-  }
-
-  render() {
- 
+  render() { 
     return (
         <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight}}>
-          <MenuNavigator />
+          <MainNavigator />
         </View>
     );
   }
