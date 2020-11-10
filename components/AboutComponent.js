41862ae7fc366ca89779578bb24a22fad0258886
minsoeaung <minsoeaung2001@gmail.com>
2020-11-10 22:01:21 +0630

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView ,FlatList, Text } from 'react-native';
 import { ListItem, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -44,18 +45,40 @@ class About extends Component {
             );
         };
 
+        if(this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        } else if(this.props.leaders.errMess) {
+            return (
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        } else {
         return (
-            <ScrollView>
-                <History />
-                <Card title="Corporate Leadership">
-                    <FlatList 
-                        data={this.props.leaders.leaders}
-                        renderItem={renderLeaderItem}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                </Card>              
-            </ScrollView>
-        );
+                <ScrollView>
+                    <History />
+                    <Card title="Corporate Leadership">
+                        <FlatList 
+                            data={this.props.leaders.leaders}
+                            renderItem={renderLeaderItem}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>              
+                </ScrollView>
+            );
+        }
     }
 }
 
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -3,6 +3,7 @@ import { FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux'
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -17,7 +18,9 @@ class Menu extends Component {
     }
 
     render() {
+
         const { navigate } = this.props.navigation;
+
         const renderMenuItem = ({item, index}) => {
             return (
                 <Tile
@@ -31,6 +34,18 @@ class Menu extends Component {
             );
         };
 
+        if(this.props.dishes.isLoading) {
+            return (
+                <Loading />
+            );
+        } else if(this.props.dishes.errMess) {
+            return (
+                <View>
+                    <Text>{props.dishes.errMess}</Text>
+                </View>
+            );
+        }
+        
         return (
             <FlatList 
                 data = {this.props.dishes.dishes}
