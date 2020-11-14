df3a1e3e3f6c2e1d74dc8934d1525bf6585f4523
minsoeaung <minsoeaung2001@gmail.com>
2020-11-14 21:23:46 +0630

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from "expo";
 
 class Reservation extends Component {
     constructor(props) {
@@ -12,7 +14,48 @@ class Reservation extends Component {
             date: ''
         }
     };
-    
+
+    static navigationOptions = {
+        title: 'Reserve Table'
+    };
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        } else {
+            if (Platform.OS === "android") {
+                Notifications.createChannelAndroidAsync("notify", {
+                  name: "notify",
+        
+                  sound: true,
+        
+                  vibrate: true,
+                });
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
+
     handleReservation = () => {
         console.log(JSON.stringify(this.state));
         Alert.alert(
@@ -20,7 +63,10 @@ class Reservation extends Component {
             'Number of Guests: '+this.state.guests+'\n'+'Smoking?: '+this.state.smoking +'\n'+'Date and Time: '+this.state.date,
             [
                 {text: 'Cancel', onPress: () => this.resetForm(), style: 'cancel'},
-                {text: 'OK', onPress: () => this.resetForm()}
+                {text: 'OK', onPress: () => {
+                    this.presentLocalNotification(this.state.date); 
+                    this.resetForm()
+                }}
             ],
             { cancelable: false }
         );
@@ -33,10 +79,6 @@ class Reservation extends Component {
             date: ''
         });
     }
-    
-    static navigationOptions = {
-        title: 'Reserve Table'
-    };
 
     render() {
         return (
